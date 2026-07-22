f9c64677aaaac80ba51bf7d61f683c53a3843759
Miguel <devmig1108@gmail.com>
2026-07-22 16:06:09 -0600

    form testing

--- includes/header.php
+++ includes/header.php
@@ -24,6 +24,9 @@
   <link rel="stylesheet" href="/styles.css">
   <script src="animations.js" defer></script>
 
+  <link rel="icon" type="image/png" sizes="32x32" href="/assets/rs-logo.png">
+  <link rel="icon" type="image/x-icon" href="/assets/rs-logo.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="/assets/rs-logo.png">
   <script type="application/ld+json">
   {
     "@context": "https://schema.org",
