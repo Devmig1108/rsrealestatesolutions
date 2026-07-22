bbc45c7dd4393040ed820ca0b1e585e0ce6f93b3
Miguel <devmig1108@gmail.com>
2026-07-22 15:16:25 -0600

    form testing

--- config/secure_env.php
+++ config/secure_env.php
@@ -6,7 +6,7 @@ define('TURNSTILE_SITE_KEY', '0x4AAAAAAD7eBk33ha0ElhS4');
 define('TURNSTILE_SECRET_KEY', '0x4AAAAAAD7eBjx3TlV5FSuook3gOccKzUE');
 
 // 2. Zoho ZeptoMail (Transactional Email API)
-define('ZEPTO_API_KEY', 'Zoho-enczapikey wSsVR613/kX4Bqp4zmeqJ7swmQkDDlr2RER/0QGiv376TKzK8cc7kUzLDQ6uT/UWQmFuHTRBoOh/y0wE1DIJjdQuz1oDDiiF9mqRe1U4J3x17qnvhDzMX2VZlxuIKIgNxgRrnGJjE88n+g==');
+define('ZEPTO_API_KEY', 'Zoho-enczapikey wSsVR610rxOhBvh6mzT5ce1umlxQVVmnF0h73VKnuCWvGv2Q9MdukUGYDQX0HPVKRGZpFztBpbJ4mhxT1WIGj457mQ0ECCiF9mqRe1U4J3x17qnvhDzPWm9VlxaJKo8BwA9qmGRkF8gm+g==');
 
 // 3. Form Token Secret (Prevents cross-site request forgery)
 // Generate a random 32-character string for this (e.g., using a password generator)
